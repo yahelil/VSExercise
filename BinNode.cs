c4b4acba50d15af7afa2ee477cf6556ad3c2365d
yahelil <117468179+yahelil@users.noreply.github.com>
2023-01-10 00:05:56 +0200

    Add files via upload

---
+++ BinNode.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    internal class BinNode<T>
+    {
+
+        private T value;
+        private BinNode<T> left;
+        private BinNode<T> right;
+
+        public BinNode(T value)
+        {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+        public BinNode(T value, BinNode<T> left, BinNode<T> right)
+        {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+        public T Getvalue()
+        {
+            return this.value;
+        }
+        public BinNode<T> Getleft()
+        {
+            return this.left;
+        }
+        public BinNode<T> Getright()
+        {
+            return this.right;
+        }
+        public bool Hasleft()
+        {
+            return this.left != null;
+        }
+        public bool Hasright()
+        {
+            return this.right != null;
+        }
+        public void Setvalue(T value)
+        {
+            this.value = value;
+        }
+        public void Setleft(BinNode<T> left)
+        {
+            this.left = left;
+        }
+        public void Setright(BinNode<T> right)
+        {
+            this.right = right;
+        }
+        public override string ToString()
+        {
+            return "( Left: " + this.left + ", Value:  " + this.value + ", Right: " + this.right + ")";
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    internal class Stack<T>
+    {
+        private Node<T> Head;
+
+        public Stack()
+        {
+            this.Head = null;
+        }
+        public void Push(T add)
+        {
+            Node<T> temp = new Node<T>(add);
+            temp.Setnext(Head);
+            Head = temp;
+        }
+        public T pop()
+        {
+            T x = Head.Getvalue();
+            Head = Head.Getnext();
+            return x;
+        }
+        public T Top()
+        {
+            return Head.Getvalue();
+        }
+        public bool IsEmpty()
+        {
+            return Head == null;
+        }
+        public override string ToString()
+        {
+            if (this.IsEmpty())
+            {
+                return "[]";
+            }
+            Stack<T> temp = new Stack<T>();
+            while (!this.IsEmpty())
+            {
+                temp.Push(this.pop());
+            }
+            string str = "[";
+            while (!temp.IsEmpty())
+            {
+                str += temp.Top() + ",";
+                this.Push(temp.pop());
+            }
+            str += "]";
+            return str;
+        }
+    }
+}
